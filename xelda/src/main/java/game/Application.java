b0cfa07ce8f47342780a04d32fde9a6ec248f814
Bruno Pascoal <brunompp98@gmail.com>
2022-11-23 17:09:17 +0000

    Game start

--- xelda/src/main/java/game/Application.java
+++ xelda/src/main/java/game/Application.java
@@ -2,6 +2,7 @@ package game;
 
 public class Application {
     public static void main(String[] args){
-
+        Game game = new Game();
+        game.startGame();
     }
 }
--- xelda/src/main/java/game/Game.java
+++ xelda/src/main/java/game/Game.java
@@ -1,4 +1,36 @@
 package game;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Game {
+
+    private static Screen screen;
+    public Game() {
+
+
+        try {
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+
+            screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen is necessary
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startGame() {
+
+    }
 }
