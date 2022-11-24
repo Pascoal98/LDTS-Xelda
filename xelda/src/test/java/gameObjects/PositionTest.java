37d5a75916b3d1de8af4c401781ac5634bbc6dd8
Henrique Silva <henrique@MBP-de-Henrique.lan>
2022-11-24 19:16:03 +0000

    Added pitest and tests for Position

---
+++ xelda/src/test/java/gameObjects/PositionTest.java
@@ -0,0 +1,68 @@
+package gameObjects;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PositionTest {
+
+    Position position;
+
+    @BeforeEach
+    public void helper() {
+        this.position = new Position(5, 10);
+    }
+
+    @Test
+    public void getX() {
+        Assertions.assertEquals(5, position.getX());
+    }
+
+    @Test
+    public void getY() {
+        Assertions.assertEquals(10, position.getY());
+    }
+
+    @Test
+    public void setX() {
+        position.setX(20);
+        Assertions.assertEquals(20,position.getX());
+    }
+
+    @Test
+    public void setY() {
+        position.setY(2);
+        Assertions.assertEquals(2, position.getY());
+    }
+
+    @Test
+    public void equalsTrueDiffObject() {
+        Position position2 = new Position(5, 10);
+        Assertions.assertTrue(position.equals(position2));
+    }
+
+    @Test
+    public void equalsTrueSameObject() {
+        Assertions.assertTrue(position.equals(position));
+    }
+
+    @Test
+    public void equalsFalseDiffObject() {
+        Position position2 = new Position(5, 20);
+        Assertions.assertFalse(position.equals(position2));
+        Position position3 = new Position(20, 10);
+        Assertions.assertFalse(position.equals(position3));
+    }
+
+    @Test
+    public void equalsFalseNull() {
+        Position pos = null;
+        Assertions.assertFalse(position.equals(pos));
+    }
+
+    @Test
+    public void equalsFalseDiffClass() {
+        Wall wall = new Wall(5,10);
+        Assertions.assertFalse(position.equals(wall));
+    }
+}
