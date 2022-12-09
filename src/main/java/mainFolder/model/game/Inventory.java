2501810e73518d4b7339b01effeb8696e56fdcf8
Henrique Silva <henrique@MacBook-Pro-de-Henrique.local>
2022-12-09 15:08:49 +0000

    Woking in Inventory

--- src/main/java/mainFolder/model/game/Inventory.java
+++ src/main/java/mainFolder/model/game/Inventory.java
@@ -1,4 +1,54 @@
 package mainFolder.model.game;
 
+import mainFolder.model.game.items.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inventory {
+    private final static int MAX_SIZE = 10;
+    private int size = 0;
+    private List<Item> items;
+
+    public Inventory() {
+        this.items = new ArrayList<>();
+    }
+
+    public boolean itemInInventory(Item item) {
+        return items.contains(item);
+    }
+
+
+    //TODO: display message saying inventory is full
+    public void addItem(Item item) {
+        if(size >= Inventory.MAX_SIZE)
+            return;
+        items.add(item);
+        size++;
+    }
+
+    public Item getItem(Item item) {
+        if(itemInInventory(item)) {
+            for(Item i : items) {
+                if(item.equals(i))
+                    return i;
+            }
+        }
+        return null;
+    }
+
+    public void useItem(Item item) {
+        if(itemInInventory(item)) {
+            items.remove(item);
+            size--;
+        }
+    }
+
+    public List<Item> getAllItems() {
+        List<Item> it = new ArrayList<>();
+        for(Item i : items) {
+            it.add(i);
+        }
+        return it;
+    }
 }
--- src/main/java/mainFolder/model/game/elements/Hero.java
+++ src/main/java/mainFolder/model/game/elements/Hero.java
@@ -1,6 +1,7 @@
 package mainFolder.model.game.elements;
 
 import mainFolder.model.game.Inventory;
+import mainFolder.model.game.items.Item;
 
 public class Hero extends Element{
     private int MAX_HEALTH = 10;
@@ -12,6 +13,17 @@ public class Hero extends Element{
         this.inventory = new Inventory();
     }
 
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public static int setMaxHealth() {
+
+    }
+    public int getMaxHealth() {
+        return max;
+    }
+
     public void decreaseHealth() {
         this.health--;
     }
@@ -20,4 +32,11 @@ public class Hero extends Element{
         return health;
     }
 
+    public void addItem(Item item) {
+        inventory.addItem(item);
+    }
+
+    public void useItem(Item item) {
+        inventory.useItem(item);
+    }
 }
