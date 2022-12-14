32cfc2860c50580d6510fcc077cc90ca01cc54eb
Henrique Silva <henrique@MBP-de-Henrique.lan>
2022-12-14 22:54:39 +0000

    Added game over menu when hero dies

---
+++ src/main/java/mainFolder/model/menu/GameOverMenu.java
@@ -0,0 +1,18 @@
+package mainFolder.model.menu;
+
+import java.util.Arrays;
+
+public class GameOverMenu extends Menu {
+
+    public GameOverMenu() {
+        super(Arrays.asList("Retry", "Exit"));
+    }
+
+    public boolean isSelectedRetry() {
+        return isSelected(0);
+    }
+
+    public boolean isSelectedExit() {
+        return isSelected(1);
+    }
+}
