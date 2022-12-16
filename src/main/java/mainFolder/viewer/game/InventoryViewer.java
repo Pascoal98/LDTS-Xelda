3ebb39e1513b0968d092430fe153fdd7c6195ffa
Henrique Silva <henrique@MBP-de-Henrique.lan>
2022-12-16 00:24:11 +0000

    upgraded some menus

--- src/main/java/mainFolder/viewer/game/InventoryViewer.java
+++ src/main/java/mainFolder/viewer/game/InventoryViewer.java
@@ -15,11 +15,11 @@ public class InventoryViewer extends Viewer<InventoryMenu> {
     protected void drawElements(GUI gui) {
         gui.drawText(new Position(5, 2), "Inventory", "#FFFFFF");
         gui.drawText(new Position(5, 4), getModel().stringHeroHealth(), "#FFFFFF");
-        gui.drawText(new Position(5,5), getModel().stringInventorySpace(), "#FFFFFF");
+        gui.drawText(new Position(5,6), getModel().stringInventorySpace(), "#FFFFFF");
 
         for(int i = 0; i < getModel().getNumberEntries(); i++)
             gui.drawText(
-                    new Position(5, 7+i),
+                    new Position(5, 8+i),
                     getModel().addString(i),
                     getModel().isSelected(i) ? "#FFD700" : "#FFFFFF"
             );
