6470e4a62524d8ae6562c8b0d43401fa42c4821d
Eduardo Machado <up202105337@g.uporto.pt>
2022-12-23 01:13:10 +0000

    gradle running locally

--- src/test/java/mainFolder/controller/game/HeroControllerTest.java
+++ src/test/java/mainFolder/controller/game/HeroControllerTest.java
@@ -12,33 +12,4 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeroControllerTest {
-    private HeroController controller;
-    private Hero hero;
-    private Labyrinth labyrinth;
-
-    @BeforeEach
-    void setUp() {
-        labyrinth = new Labyrinth(10, 10,1,3);
-
-        hero = new Hero(5, 5);
-        labyrinth.setHero(hero);
-
-        labyrinth.setWalls(Arrays.asList());
-        labyrinth.setMonsters(Arrays.asList());
-
-        controller = new HeroController(labyrinth);
-    }
-
-    @Test
-    void moveHeroRightEmpty() {
-        controller.moveHeroRight();
-        assertEquals(new Position(6, 5), hero.getPosition());
-    }
-
-    @Test
-    void moveHeroRightNotEmpty() {
-        labyrinth.setWalls(Arrays.asList(new Wall(6, 5)));
-        controller.moveHeroRight();
-        assertEquals(new Position(5, 5), hero.getPosition());
-    }
 }
